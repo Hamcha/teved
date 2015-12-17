d5ececf97ef8b9dcf13a8496090174377a1f5257
Hamcha <zikyky@gmail.com>
2015-12-17 20:24:46 +0100

    Functional X and _ buttons

--- app/Chrome.jsx
+++ app/Chrome.jsx
@@ -3,37 +3,52 @@
 import React  from "react";
 import styles from "./Chrome.module.scss";
 
+const close = function() {
+	"use strict";
+	const app = require('electron').remote.app;
+	app.quit();
+}
+
+const minimize = function() {
+	"use strict";
+	require('electron').remote.getCurrentWindow().minimize();
+}
+
 class TBButton extends React.Component {
-    render(): any {
-        return <div className={styles.tbButton} style={this.props.style}>{this.props.children}</div>
-    }
+	render(): any {
+		return <div className={styles.tbButton}
+		            style={this.props.style}
+		            onClick={this.props.action}>
+				{this.props.children}
+		</div>;
+	}
 }
 
 class TitleBar extends React.Component {
-    render(): any {
-        return <div className={styles.titleBar}>
-            <div className={styles.titleText}>TEVed - TEV Editor</div>
-            <div className={styles.btnContainer}>
-                <TBButton style={{"font-weight": "bold"}}>&#8212;</TBButton>
-                <div className={styles.hlbtn}>
-                    <TBButton>&#10006;</TBButton>
-                </div>
-            </div>
-        </div>;
-    }
+	render(): any {
+		return <div className={styles.titleBar}>
+			<div className={styles.titleText}>TEVed - TEV Editor</div>
+			<div className={styles.btnContainer}>
+				<TBButton action={minimize} style={{"fontWeight": "bold"}}>&#8212;</TBButton>
+				<div className={styles.hlbtn}>
+					<TBButton action={close}>&#10006;</TBButton>
+				</div>
+			</div>
+		</div>;
+	}
 }
 
 class Chrome extends React.Component {
-    render(): any {
-        return <div className={styles.rootWnd}>
-            <TitleBar />
-            <div className={styles.uiWrapper}>
-                <div className={styles.uiContainer}>
-                    {this.props.children}
-                </div>
-            </div>
-        </div>;
-    }
+	render(): any {
+		return <div className={styles.rootWnd}>
+			<TitleBar />
+			<div className={styles.uiWrapper}>
+				<div className={styles.uiContainer}>
+					{this.props.children}
+				</div>
+			</div>
+		</div>;
+	}
 }
 
 export default Chrome;
